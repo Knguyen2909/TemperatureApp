92150715f9a87e8a9b5c1c9b2ff1788f640c63f3
Knguyen2909 <knguy2909@gmail.com>
2023-04-20 17:48:09 +0100

    Added Schedule function

--- src/pages/ScheduleManager/ScheduleManager.js
+++ src/pages/ScheduleManager/ScheduleManager.js
@@ -1,46 +1,14 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from 'react-native';
-import { Calendar, Agenda } from 'react-native-calendar-timetable';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 
-const ScheduleManager = () => {
-  const events = [
-    {
-      title: 'Event 1',
-      start: '2023-04-26 10:00:00',
-      end: '2023-04-26 11:00:00',
-      color: '#bce6eb',
-    },
-    {
-      title: 'Event 2',
-      start: '2023-04-27 14:00:00',
-      end: '2023-04-27 16:00:00',
-      color: '#ffe0b2',
-    },
-    {
-      title: 'Event 3',
-      start: '2023-04-28 09:30:00',
-      end: '2023-04-28 10:30:00',
-      color: '#ffccbc',
-    },
-  ];
+function ScheduleManager() {
+  const [value, onChange] = useState(new Date());
 
   return (
-    <View style={styles.container}>
-      <Calendar
-        events={events}
-        onEventPress={(event) => console.log(event)}
-        dayFormat="ddd"
-        headerDateFormat="MMM DD"
-      />
-      <Agenda events={events} />
-    </View>
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+}
 
 export default ScheduleManager;
