00077e2f19c76c28c97ae871154677260acd988a
Knguyen2909 <knguy2909@gmail.com>
2023-04-22 20:06:01 +0100

    formatted the scheduler and changed the bg colour

--- src/pages/ScheduleManager/ScheduleManager.js
+++ src/pages/ScheduleManager/ScheduleManager.js
@@ -1,23 +1,50 @@
 import React from "react";
 import ScheduleSelector from 'react-schedule-selector'
+import "./ScheduleManagerstyles.css";
+
 
 class ScheduleManager extends React.Component {
-  state = { schedule : [] }
+  state = { schedule: [] }
 
   handleChange = newSchedule => {
     this.setState({ schedule: newSchedule })
   }
 
+
+
   render() {
+
+    function alertTemp() {
+      alert("The settings have been confirmed")
+    };
+
     return (
-      <ScheduleSelector
-        selection={this.state.schedule}
-        numDays={7}
-        minTime={1}
-        maxTime={24}
-        hourlyChunks={1}
-        onChange={this.handleChange}
-      />
+      <div className="app-container">
+ 
+        <div className="temperature-display-container">
+          
+          <div>
+            
+          <div className="top-border"> </div>
+            <ScheduleSelector
+              selection={this.state.schedule}
+              numDays={7}
+              minTime={1}
+              maxTime={24}
+              hourlyChunks={1}
+              dateFormat="dd"
+              onChange={this.handleChange}
+            />
+            
+          </div>
+          
+        </div>
+        <div className="top-border"> </div>
+        <div className="start-button">
+          
+          <start-button onClick={alertTemp}>Confirm</start-button>
+        </div>
+      </div>
     )
   }
 }
